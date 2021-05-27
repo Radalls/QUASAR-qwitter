b9d61b3ab86542632a41a1ccb6a530f7675d0063
Radalls <leosallard@gmail.com>
2021-05-27 12:25:06 +0200

    a

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,47 +1,37 @@
 <template>
   <q-layout view="lHr lpR fFf">
-
     <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="left = !left" />
-
         <q-toolbar-title class="text-weight-bold">
           <span class="gt-sm">{{ $route.name }}</span>
           <q-icon class="header-icon q-pa-md lt-md" name="fas fa-dove" size="sm" color="primary" />
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
-
     <q-drawer show-if-above v-model="left" side="left" :width="283" bordered>
       <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
-
       <q-list>
         <q-item to="/" clickable v-ripple exact>
           <q-item-section avatar>
             <q-icon name="home" size="md" />
           </q-item-section>
-
           <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
         </q-item>
-
         <q-item to="/about" clickable v-ripple exact>
           <q-item-section avatar>
             <q-icon name="help" size="md" />
           </q-item-section>
-
           <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
-
     <q-drawer show-if-above v-model="right" side="right" bordered>
       <q-input placeholder="Search Qwitter" class="q-ma-md" outlined rounded dense>
         <template v-slot:prepend>
           <q-icon name="search" />
         </template>
       </q-input>
-
       <q-list separator padding>
         <q-item class="q-pa-md">
           <q-item-section>
@@ -49,42 +39,35 @@
             <q-item-label class="text-weight-bold">Teachers are cancelled !</q-item-label>
             <q-item-label caption lines="2">random Twitter shit.</q-item-label>
           </q-item-section>
-
           <q-item-section side top>
             <q-item-label caption>5 min ago</q-item-label>
           </q-item-section>
         </q-item>
-
         <q-item class="q-pa-md">
           <q-item-section>
             <q-item-label class="text-grey" overline>Sport</q-item-label>
             <q-item-label class="text-weight-bold">Saint-Jean les Fossés l'emporte contre Triffouillis les Oies</q-item-label>
             <q-item-label caption lines="2">Une rencontre tendue du cul</q-item-label>
           </q-item-section>
-
           <q-item-section side top>
             <q-item-label caption>5 min ago</q-item-label>
           </q-item-section>
         </q-item>
-
         <q-item class="q-pa-md">
           <q-item-section>
             <q-item-label class="text-grey" overline>Litterature</q-item-label>
             <q-item-label class="text-weight-bold">Le nouvel opus des aventures de Martine</q-item-label>
             <q-item-label caption lines="2">Martine et 30 cm de bonheur dans toutes les librairies</q-item-label>
           </q-item-section>
-
           <q-item-section side top>
             <q-item-label caption>5 min ago</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,62 +1,85 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <div class="col">
-        <q-input class="new-qweet" v-model="newQweetContent" placeholder="What's happening?" maxlength="280" bottom-slots counter autogrow>
-          <template v-slot:before>
-            <q-avatar size="xl">
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input class="new-qweet" v-model="newQweetContent" placeholder="What's happening?" maxlength="280" bottom-slots counter autogrow>
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="../assets/lion.png">
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn @click="addNewQweet" class="q-mb-lg" color="primary" label="Qweet" :disable="!newQweetContent" unelevated rounded no-caps />
+        </div>
+      </div>
+      <q-separator class="divider" size="10px" color="grey-2" />
+      <q-list separator>
+        <transition-group appear enter-active-class="animated fadeIn slow" leave-active-class="animated fadeOut slow">
+          <q-item v-for="qweet in qweets" :key="qweet.date" class="qweet q-py-md">
+            <q-item-section avatar top>
+              <q-avatar size="xl">
               <img src="../assets/lion.png">
-            </q-avatar>
-          </template>
-        </q-input>
-      </div>
-
-      <div class="col col-shrink">
-        <q-btn class="q-mb-lg" color="primary" label="Qweet" :disable="!newQweetContent" unelevated rounded no-caps />
-      </div>
-    </div>
-
-    <q-separator class="divider" size="10px" color="grey-2" />
-
-    <q-list>
-      <q-item class="q-py-md">
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img src="../assets/lion.png">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label class="text-subtitle1">
-            <strong>Radalls</strong>
-            <span class="text-grey-7">@le_chibrachiosaure</span>
-          </q-item-label>
-          <q-item-label class="qweet-content text-body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vel turpis eu nibh consequat lobortis eget at turpis.
-            Vivamus id quam sit amet massa egestas sodales id ut felis.
-          </q-item-label>
-          <div class="qweet-icons row justify-between q-mt-sm">
-            <q-btn flat round color="grey" icon="far fa-comment" size="sm" />
-            <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
-            <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
-            <q-btn flat round color="grey" icon="fas fa-trash" size="sm" />
-          </div>
-        </q-item-section>
-
-        <q-item-section side top>
-          1 min ago
-        </q-item-section>
-      </q-item>
-    </q-list>
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>Radalls</strong>
+                <span class="text-grey-7">@le_chibrachiosaure <br class="lt-md"> &bull; {{ qweet.date | relativeDate }}</span>
+              </q-item-label>
+              <q-item-label class="qweet-content text-body1">{{ qweet.content }}</q-item-label>
+                <div class="qweet-icons row justify-between q-mt-sm">
+                  <q-btn flat round color="grey" icon="far fa-comment" size="sm" />
+                  <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
+                  <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+                  <q-btn @click="deleteQweet(qweet)" flat round color="grey" icon="fas fa-trash" size="sm" />
+                </div>
+            </q-item-section>
+          </q-item>
+        </transition-group>
+      </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
 <script>
+import { formatDistance } from 'date-fns'
+
 export default {
   name: 'PageHome',
   data() {
     return {
-      newQweetContent: ''
+      newQweetContent: '',
+      qweets: [
+        {
+          content: 'Oui.',
+          date: 1622104134442
+        },
+        {
+          content: 'Non.',
+          date: 1622104227438
+        }
+      ]
+    }
+  },
+  methods: {
+    addNewQweet() {
+      let newQweet = {
+        content: this.newQweetContent,
+        date : Date.now()
+      }
+      this.qweets.unshift(newQweet)
+      this.newQweetContent = ''
+    },
+    deleteQweet(qweetToDelete) {
+      this.qweets.splice(this.qweets.findIndex(qweet => qweet.date === qweetToDelete.date), 1)
+    }
+  },
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date())
     }
   }
 }
@@ -71,6 +94,8 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+.qweet:not(:first-child)
+  border-top: 1px solid rgba(0, 0, 0, 0.12)
 .qweet-content
   white-space: pre-line
 .qweet-icons
